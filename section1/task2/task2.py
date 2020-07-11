2aaf12e410c167d8e34b19d83520a8f9302d4a36
Deyan Hao <deyanh@bu.edu>
2020-07-11 04:53:39 -0400

    update

--- section1/task2/task2.py
+++ section1/task2/task2.py
@@ -1,32 +1,31 @@
 from binance.client import Client
-from binance.exceptions import BinanceRequestException, BinanceAPIException, BinanceOrderException
+from binance.exceptions import BinanceAPIException
 import pandas as pd
-
 
 class Binance:
 
-    @staticmethod
-    def get_candle(baseAsset, quoteAsset, interval, start, end='now'):
-        # baseAsset: Name of symbol (str)
-        # quoteAsset: Name of symbol (str)
+    def __init__(self, api_key, api_secret):
+        self.client = Client(api_key, api_secret)
+
+    def get_candle(self, symbol, interval, start, end='now'):
         # interval: eg:'1d','1h','30m' (str)
         # start: start time (str)
         # end: end time (str)
         #      default to now
 
         client = Client("", "")
-        symbol = baseAsset+quoteAsset
         try:
             klines = client.get_historical_klines(symbol, interval, start, end)
         except BinanceAPIException as e:
             print(e.message)
             return
-        except BinanceRequestException as e:
-            print(e.message)
-            return
-        df = pd.DataFrame(klines)
-        df.columns = ['Open time', 'Open', 'High', 'Low', 'Close', baseAsset+' Volume', 'Close time', quoteAsset+' Volume',
-                      'Number of trades', 'Taker buy'+baseAsset+' volume', 'Taker buy'+quoteAsset+' volume', 'Ignore']
+        self.klines = klines
+        return klines
+
+    def format_candle(self):
+        df = pd.DataFrame(self.klines)
+        df.columns = ['Open time', 'Open', 'High', 'Low', 'Close', 'BaseAsset Volume', 'Close time', 'QuoteAsset Volume',
+                      'Number of trades', 'Taker buy baseAsset volume', 'Taker buy quoteAsset volume', 'Ignore']
         df['Open time'] = pd.to_datetime(df['Open time'], unit='ms')
         df['Close time'] = pd.to_datetime(df['Close time'], unit='ms')
         closetime = df.pop('Close time')
@@ -35,68 +34,53 @@ class Binance:
         df.to_csv('section1/task2/CandleData.csv', index=0)
         return
 
-    @staticmethod
-    def get_transactions(baseAsset, quoteAsset, number=500):
-        # baseAsset: Name of symbol (str)
-        # quoteAsset: Name of symbol (str)
+    def get_transactions(self, symbol, number=500):
         # number: # of trades wish to check (int)
         #         default:500 max:500
         # most recent trades show first
 
         client = Client("", "")
-        param = baseAsset+quoteAsset
         try:
-            trades = client.get_recent_trades(symbol=param, limit=number)
+            trades = client.get_recent_trades(symbol=symbol, limit=number)
         except BinanceAPIException as e:
             print(e.message)
             return
-        except BinanceRequestException as e:
-            print(e.message)
-            return
-        df = pd.DataFrame(trades)
+        self.trades = trades
+        return trades
+
+    def format_trade(self):
+        df = pd.DataFrame(self.trades)
         df.pop('id')
         time = df.pop('time')
         df.insert(0, time.name, time)
         df = df.sort_values(by='time', ascending=False)
         df['time'] = pd.to_datetime(df['time'], unit='ms')
-        df = df.rename(
-            {'qty': baseAsset+' Qty', 'quoteQty': quoteAsset+' Qty'}, axis=1)
         df.to_csv('section1/task2/TradesData.csv', index=0)
         return
 
-    @staticmethod
-    def get_marketdepth(baseAsset, quoteAsset, number=100):
-        # baseAsset: Name of symbol (str)
-        # quoteAsset: Name of symbol (str)
+    def get_marketdepth(self, symbol, number=100):
         # number: # wish to check (int)
         #         default:100 max:1000
 
         client = Client("", "")
-        param = baseAsset+quoteAsset
         try:
-            trades = client.get_order_book(symbol=param, limit=number)
+            depth = client.get_order_book(symbol=symbol, limit=number)
         except BinanceAPIException as e:
             print(e.message)
             return
-        except BinanceRequestException as e:
-            print(e.message)
-            return
-        bids, asks = pd.DataFrame(trades['bids']), pd.DataFrame(trades['asks'])
-        bids.columns, asks.columns = ['price', 'qty'], ['price', 'qty']
-        bids, asks = bids.stack(), asks.stack()
+        self.depth = depth
+        return depth
+
+    def formant_depth(self):
+        data = self.depth
+        bids, asks = pd.DataFrame(data['bids']), pd.DataFrame(data['asks'])
+        bids.columns, asks.columns = ['Bids Price', 'Bids Qty'], ['Ask Price', 'Ask Qty']
         df = pd.concat([bids, asks], axis=1)
-        df.columns = ['bids', 'asks']
-        df.to_csv('section1/task2/MarketDepth.csv')
+        df.to_csv('section1/task2/MarketDepth.csv', index = 0)
         return
 
-# Binance.get_candle('BNB','BTC','1h','2020-06-01')
-# Binance.get_transactions('BTC','USDT')
-# Binance.get_marketdepth('BNB','BTC')
 
-    @staticmethod
-    def test_order(baseAsset, quoteAsset, action, type, **kwargs):
-        # baseAsset: Name of symbol (str)
-        # quoteAsset: Name of symbol (str)
+    def test_order(self, symbol, action, type, **kwargs):
         # action: SELL or BUY (str)
         # type: LIMIT, MARKET, STOP_LOSS, STOP_LOSS_LIMIT, TAKE_PROFIT, TAKE_PROFIT_LIMIT, LIMIT_MAKER (str)
         # timeInForce: GTC, IOC, FOK (str) 
@@ -117,18 +101,18 @@ class Binance:
         # TAKE_PROFIT_LIMIT need: timeInForce, quantity, price, stopPrice
         # LIMIT_MAKER need: quantity, price
 
-        APIkey = 'Eb3UUw08LomMLXspNtExUyBZXfdo1zdbmSAshG5SyBJxkFqczgzv4uPUFhy36xpQ'
-        secretkey = 'NWfXZWn8VBithCgMgVLX24deME7Res05lnoRBjKXu1Rg02jlKYYBB5CNSZtF9L6a'
-        client = Client(APIkey, secretkey)
-
         try:
-             client.create_test_order(symbol=baseAsset+quoteAsset, side=action, type = type, **kwargs)
-        except BinanceRequestException as e:
-            print(e.message)
-            return
+            self.client.create_test_order(symbol=symbol, side=action, type = type, **kwargs)
         except BinanceAPIException as e:
             print(e.message)
             return
-        except BinanceOrderException as e:
-            print(e.message)
-            return
+
+Binance = Binance('','')
+Binance.get_candle('BNBBTC','1h','2020-06-01')
+Binance.format_candle()
+
+Binance.get_transactions('BTCUSDT')
+Binance.format_trade()
+
+Binance.get_marketdepth('BTCUSDT')
+Binance.formant_depth()
